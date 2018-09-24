9ca9c592e3fa8bea60ca89b64ade72404898e8b1
hidx1 <43450143+hidx1@users.noreply.github.com>
2018-09-24 16:01:24 +0700

    Add files via upload

---
+++ MainCalc.java
@@ -0,0 +1,14 @@
+class MainCalc{
+  MainCalc(){
+  }
+
+  void gauss(){
+    System.out.println("Nyoba gauss yaa?");
+  }
+  void gaussjordan(){
+    System.out.println("Sekarang coba gaussjordan?");
+  }
+  void interpolasi(){
+    System.out.println("Lagi coba interpolasi?");
+  }
+}
